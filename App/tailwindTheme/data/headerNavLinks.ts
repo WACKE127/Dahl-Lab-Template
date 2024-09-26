fd23bf1a2051f2ee131b948763ec0f11434b324b
WACKE127 <tylerwacker2021@gmail.com>
2024-09-26 11:27:28 -0500

    Tailwind Theme + Minor Tailoring

---
+++ App/tailwindTheme/data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/staff', title: 'Staff' },
+  { href: '/publications', title: 'Publications' },
+  { href: '/about', title: 'About' },
+]
+
+export default headerNavLinks
